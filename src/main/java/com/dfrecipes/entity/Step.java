fb92fd1a2a6796aee9152022d6c8435557cf13c0
Joseph Denman <denmanjoseph92@Josephs-MBP.home>
2023-07-14 12:40:39 -0700

    uncomment

--- src/main/java/com/dfrecipes/entity/Step.java
+++ src/main/java/com/dfrecipes/entity/Step.java
@@ -1,54 +1,54 @@
-//package com.dfrecipes.entity;
-//
-//import javax.persistence.Column;
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//import javax.persistence.Table;
-//
-//@Entity
-//@Table
-//public class Step {
-//	
-//	@Id
-//	@GeneratedValue(strategy=GenerationType.IDENTITY)
-//	@Column(name= "id")
-//	private int id;
-//	
-//	@Column(name = "description")
-//	private String description;
-//	
-//	public Step() {
-//		super();
-//	}
-//	
-//	public Step(int id, String description) {
-//		super();
-//		this.id = id;
-//		this.description = description;
-//	}
-//	
-//	
-//	public int getId() {
-//		return id;
-//	}
-//	
-//	public void setId(int id) {
-//		this.id = id;
-//	}
-//	
-//	public String getDescription() {
-//		return description;
-//	}
-//	
-//	public void setDescription(String description) {
-//		this.description = description;
-//	}
-//	
-//	@Override
-//	public String toString() {
-//		return "Steps [id=" + id + ", description=" + description + "]";
-//	}
-//	
-//}
+package com.dfrecipes.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Step {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name= "id")
+	private int id;
+	
+	@Column(name = "description")
+	private String description;
+	
+	public Step() {
+		super();
+	}
+	
+	public Step(int id, String description) {
+		super();
+		this.id = id;
+		this.description = description;
+	}
+	
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	@Override
+	public String toString() {
+		return "Steps [id=" + id + ", description=" + description + "]";
+	}
+	
+}
